9c57961f7c6573ff5f1e3fda2ca97af3137ba157
y-kengo <yamaken0904@gmail.com>
2021-01-29 22:24:34 +0900

    class Room < ApplicationRecord   has_many :room_users, dependent: :destroy   has_many :users, through: :room_users     has_many :messages, dependent: :destroy    validates :name, presence: true end

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,13 @@
 class RoomsController < ApplicationController
 
   def index
+
   end
-  
+
+  def new
+    @room = Room.new
+  end
+
   def create
     @room = Room.new(room_params)
     if @room.save
@@ -11,11 +16,16 @@ class RoomsController < ApplicationController
       render :new
     end
   end
-  
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
+  end
+
   private
 
   def room_params
-    params.require(:room).permit(:name, user_ids: [])
+    params.require(:room).permit(:name, user_ids:[])
   end
-
 end
